53c2cf860d7f4f935f47ae02f97b329e9f5f44ec
Francesco Bonnì <francesco.bonni@gmail.com>
2018-09-09 01:43:38 +0200

    added fun to call when change overlay

--- app/src/main/java/francescobonni/checkbugpermissiondispatcher/MainActivity.kt
+++ app/src/main/java/francescobonni/checkbugpermissiondispatcher/MainActivity.kt
@@ -1,12 +1,25 @@
 package francescobonni.checkbugpermissiondispatcher
 
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.support.annotation.RequiresApi
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun showMessage() {
+        Toast.makeText(this,
+                if(Settings.canDrawOverlays(this))
+                    "can draw overlays : true"
+                else
+                    "can draw overlays : false", Toast.LENGTH_LONG).show()
     }
 }
